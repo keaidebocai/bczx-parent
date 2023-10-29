c1d3138e079ea7269d6e92cc489a5f35fc402b01
woaibocai <365191806@qq.com>
2023-10-29 19:24:06 +0800

    设置Spring-cache的注解EnableCache

--- bczx-service/service-product/src/main/java/top/woaibocai/bczx/product/ProductApplication8511.java
+++ bczx-service/service-product/src/main/java/top/woaibocai/bczx/product/ProductApplication8511.java
@@ -2,6 +2,7 @@ package top.woaibocai.bczx.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @program: bczx-parent
@@ -10,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2023-10-27 16:35
  **/
 @SpringBootApplication
+//开启缓存
+@EnableCaching
 public class ProductApplication8511 {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication8511.class,args);
